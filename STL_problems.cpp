10141386e788409ec712288f3ae84dfae659998e
SW-std <ovdet7472@gmail.com>
2025-12-01 00:04:33 +0900

    Update STL_problems.cpp

--- STL_problems.cpp
+++ STL_problems.cpp
@@ -2,44 +2,91 @@
 #include<vector>
 #include<algorithm>
 #include<map>
+#include<stack>
+#include<queue>
 using namespace std;
 
+
+
+//4번
+//class circle {
+//	double r;
+//public:
+//	circle(double ra) : r(ra) {}
+//	double radius() const {
+//		return r;
+//	}
+//	//priority_queue 떄문에 연산자 오버로딩이 필요했음
+//	bool operator<(const circle& other) const {
+//		return r < other.r;
+//	}
+//};
+//int main() {
+//	circle c1(1.0), c2(2.0), c3(3.0);
+//	stack<circle> st;
+//	queue<circle> qu;
+//	priority_queue<circle> pr;
+//	st.push(c1);
+//	st.push(c2);
+//	st.push(c3);
+//	qu.push(c1);
+//	qu.push(c2);
+//	qu.push(c3);
+//	pr.push(c1);
+//	pr.push(c2);
+//	pr.push(c3);
+//	while (!st.empty()) {
+//		cout << st.top().radius() << " ";// 맨 위(가장 나중에 들어간 것) 확인
+//		st.pop();// 맨 위(가장 나중에 들어간 것) 제거
+//	}
+//	cout << "\n";
+//	while (!qu.empty()) {
+//		cout << qu.front().radius() << " ";// 맨 앞(가장 먼저 들어간 것) 확인
+//		qu.pop();// 맨 앞(가장 먼저 들어간 것) 제거
+//	}
+//	cout << "\n";
+//	while (!pr.empty()) {
+//		cout << pr.top().radius() << " ";// 가장 우선순위가 높은 요소 확인
+//		pr.pop();// 가장 우선순위가 높은 요소 ㅈ제
+//	}
+//	cout << "\n";
+//}
 //3번
-int main() {
-    map<string, string> m;   // key: 이름, value: 전화번호
-    int n;
-
-    cout << "저장할 사람 수를 입력하세요: ";
-    cin >> n;
-
-    string name, number;
-
-    // 이름과 전화번호를 map에 저장
-    for (int i = 0; i < n; i++) {
-        cout << "\n이름 입력: ";
-        cin >> name;
-        cout << "전화번호 입력: ";
-        cin >> number;
-
-        m[name] = number;  // map은 key가 같으면 자동으로 덮어씀 이전의 정보는 저장되지않음
-    }
-
-    // 검색
-    cout << "\n검색할 이름을 입력하세요: ";
-    cin >> name;
-
-    // map에서 이름 찾기
-    auto it = m.find(name);
-
-    if (it != m.end()) {
-        cout << name << "의 전화번호는 " << it->second << " 입니다.\n";
-    }
-    else {
-        cout << "해당 이름은 전화번호부에 존재하지 않습니다.\n";
-    }
-
-    return 0;
-}
+//int main() {
+//    map<string, string> m;   // key: 이름, value: 전화번호
+//    int n;
+//
+//    cout << "저장할 사람 수: ";
+//    cin >> n;
+//
+//    string name, number;
+//
+//    // 이름과 전화번호를 map에 저장
+//    for (int i = 0; i < n; i++) {
+//        cout << "\n이름 입력: ";
+//        cin >> name;
+//        cout << "전화번호 입력: ";
+//        cin >> number;
+//
+//        m[name] = number;  // map은 key가 같으면 자동으로 덮어씀 이전의 정보는 저장되지않음
+//    }
+//
+//    // 검색
+//    cout << "\n검색할 이름을 입력하세요: ";
+//    cin >> name;
+//
+//    // map에서 이름 찾기
+//    auto it = m.find(name);
+//
+//    if (it != m.end()) {
+//        cout << name << "의 전화번호는 " << it->second << " 입니다.\n";
+//    }
+//    else {
+//        cout << "해당 이름은 전화번호부에 존재하지 않습니다.\n";
+//    }
+//
+//    return 0;
+//}
 //2번
 // int main() {
 //    map<int, int> freq;       // key: 입력 정수, value: 등장 횟수
